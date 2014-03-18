5d10fc88cae8ec2bbc71f6f7226d4e606447fbda
GMU Student <student@ubuntu.(none)>
2014-03-17 23:37:49 -0400

    added openrave 2dcam

---
+++ openrave2dArmCam.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2014 Daniel M. Lofaro (dan@danlofaro.com)
+# Modified from 2009-2011 Rosen Diankov (rosen.diankov@gmail.com)
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Shows how to use the TranslationXY2D planar translation inverse kinematics type for an arm with few joints.
+
+.. examplepre-block:: tutorial_iktranslation2d
+
+.. examplepost-block:: tutorial_iktranslation2d
+"""
+from __future__ import with_statement # for python 2.5
+__author__ = 'Daniel M. Lofaro'
+
+import time
+import openravepy
+if not __openravepy_build_doc__:
+    from openravepy import *
+    from numpy import *
+
+def main(env,options):
+    "Main example code."
+    env.Load(options.scene)
+    robot = env.GetRobots()[0]
+    if options.manipname is not None:
+        robot.SetActiveManipulator(options.manipname)
+
+    # generate the ik solver
+    ikmodel = databases.inversekinematics.InverseKinematicsModel(robot, iktype=IkParameterization.Type.TranslationXY2D)
+    if not ikmodel.load():
+        ikmodel.autogenerate()
+
+    while True:
+        with env:
+            d = [0,1.57,0]
+            robot.SetDOFValues(d,ikmodel.manip.GetArmIndices())
+            env.UpdatePublishedBodies()
+            time.sleep(0.05)
+        h=None
+
+from optparse import OptionParser
+from openravepy.misc import OpenRAVEGlobalArguments
+
+@openravepy.with_destroy
+def run(args=None):
+    """Command-line execution of the example.
+
+    :param args: arguments for script to parse, if not specified will use sys.argv
+    """
+    parser = OptionParser(description='Shows how to use different IK solutions for arms with few joints.')
+    OpenRAVEGlobalArguments.addOptions(parser)
+    parser.add_option('--scene',action="store",type='string',dest='scene',default='tridoftable.env.xml',
+                      help='Scene file to load (default=%default)')
+    parser.add_option('--manipname',action="store",type='string',dest='manipname',default=None,
+                      help='name of manipulator to use (default=%default)')
+    (options, leftargs) = parser.parse_args(args=args)
+    OpenRAVEGlobalArguments.parseAndCreateThreadedUser(options,main,defaultviewer=True)
+
+if __name__ == "__main__":
+    run()
