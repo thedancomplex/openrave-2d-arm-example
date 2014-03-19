29f71aced197994e1f2ae28fa7e691e6378a8d93
GMU Student <student@ubuntu.(none)>
2014-03-19 17:21:00 -0400

    added install script not working

---
+++ installDep.sh
@@ -0,0 +1,7 @@
+sudo apt-get install python-qt4
+sudo add-apt-repository ppa:oibaf/graphics-drivers
+sudo apt-get update
+sudo apt-get build-dep mesa
+sudo add-apt-repository ppa:opencpn/opencpn
+sudo apt-get update
+sudo apt-get install opencpn
