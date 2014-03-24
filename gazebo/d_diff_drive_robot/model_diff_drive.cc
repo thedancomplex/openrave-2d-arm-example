4516d45980e849a40b597309928b18c174c59e87
student <student@student-sim.(none)>
2014-03-24 13:57:39 -0400

    now can control the wheel torque

--- gazebo/d_diff_drive_robot/model_diff_drive.cc
+++ gazebo/d_diff_drive_robot/model_diff_drive.cc
@@ -5,25 +5,66 @@
 #include <stdio.h>
 
 namespace gazebo
-{
+{   
   class ModelDiffDrive : public ModelPlugin
   {
-    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
+    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf) 
     {
+
       // Store the pointer to the model
       this->model = _parent;
 
-      // Listen to the update event. This event is broadcast every
-      // simulation iteration.
-      this->updateConnection = event::Events::ConnectWorldUpdateBegin(
-          boost::bind(&ModelDiffDrive::OnUpdate, this, _1));
+      // Load parameters for this plugin
+      if (this->LoadParams(_sdf))
+      {
+        // Listen to the update event. This event is broadcast every
+        // simulation iteration.
+        this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+            boost::bind(&ModelDiffDrive::OnUpdate, this));
+      }
+    }
+
+    public: bool LoadParams(sdf::ElementPtr _sdf) 
+    {
+      if (this->FindJointByParam(_sdf, this->left_wheel_joint_,
+                             "left_wheel_hinge") &&
+          this->FindJointByParam(_sdf, this->right_wheel_joint_,
+                             "right_wheel_hinge"))
+        return true;
+      else
+        return false;
+    }
+
+    public: bool FindJointByParam(sdf::ElementPtr _sdf,
+                                  physics::JointPtr &_joint,
+                                  std::string _param)
+    {
+      if (!_sdf->HasElement(_param))
+      {
+        gzerr << "param [" << _param << "] not found\n";
+        return false;
+      }
+      else
+      {
+        _joint = this->model->GetJoint(
+          _sdf->GetElement(_param)->GetValueString());
+
+        if (!_joint)
+        {
+          gzerr << "joint by name ["
+                << _sdf->GetElement(_param)->GetValueString()
+                << "] not found in model\n";
+          return false;
+        }
+      }
+      return true;
     }
 
     // Called by the world update start event
-    public: void OnUpdate(const common::UpdateInfo & /*_info*/)
+    public: void OnUpdate()
     {
-      // Apply a small linear velocity to the model.
-      //this->model->SetLinearVel(math::Vector3(.03, 0, 0));
+      this->left_wheel_joint_->SetForce(0, 0.2);
+      this->right_wheel_joint_->SetForce(0, -0.2);
     }
 
     // Pointer to the model
@@ -31,8 +72,11 @@ namespace gazebo
 
     // Pointer to the update event connection
     private: event::ConnectionPtr updateConnection;
+
+    private: physics::JointPtr left_wheel_joint_;
+    private: physics::JointPtr right_wheel_joint_;
   };
 
   // Register this plugin with the simulator
-  GZ_REGISTER_MODEL_PLUGIN(ModelDiffDrive);
+  GZ_REGISTER_MODEL_PLUGIN(ModelDiffDrive)
 }
